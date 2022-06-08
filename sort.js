016a2b36f297cf5c2bb6c9762e8c2f9a4b9eab62
fumionakai <iikaofumio-n219@outlook.jp>
2022-06-08 14:47:16 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,3 +1,13 @@
-sort.jsvar number = [19, 3, 81, 1, 24, 21];
+sort.jsvar sortNumber = function (number) {
+   number.sort(function (a, b) {
+       if (a == b) {
+           return 0;
+       }
+       return a < b ? -1 : 1;
+   });
+};
+
+var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
 
